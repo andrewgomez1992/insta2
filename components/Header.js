697eb09a66fecc15b355d6c83680d4ff994d6ab2
Andrew Gomez <drewgomez209@gmail.com>
2022-09-20 11:36:15 -0700

    right side header icons wip

--- components/Header.js
+++ components/Header.js
@@ -10,12 +10,14 @@ import {
   HeartIcon,
   PaperAirplaneIcon,
   MenuIcon,
+  HomeIcon,
 } from "@heroicons/react/solid";
+import { PaperAirplane } from "heroicons-react";
 
 const Header = () => {
   return (
     <div>
-      <div className="flex justify-between max-w-6xl">
+      <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
         {/* Left */}
         <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
@@ -26,7 +28,7 @@ const Header = () => {
           />
         </div>
 
-        <div className="relative w-12 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-12 lg:hidden flex-shrink-0 cursor-pointer pt-2">
           <Image
             src={iglogo}
             alt="instagram-logo"
@@ -36,20 +38,26 @@ const Header = () => {
         </div>
 
         {/* Middle - search input field */}
-        <div className="relative mt-1 p-3 rounded-md">
-          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-            <SearchIcon className="h-5 w-5 text-gray-500" />
-          </div>
-          <input
-            type="text"
-            placeholder="Search"
-            className="bg-gray-100 block 
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-gray-100 block 
             w-full pl-10 sm:text-sm border-gray-300
              focus:ring-black focus:border-black rounded-md"
-          />
+            />
+          </div>
         </div>
-
         {/* Right */}
+        <div className="flex items-center justify-end space-x-4">
+            <HomeIcon className="navBtn"/>
+            <MenuIcon className="h-6 md:hidden cursor-pointer"/>
+            <PaperAirplaneIcon className="navBtn"/>
+        </div>
       </div>
     </div>
   );
