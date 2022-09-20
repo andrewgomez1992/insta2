6012a8db1d8fff421bcf339e61423b219a6d86a6
Andrew Gomez <drewgomez209@gmail.com>
2022-09-20 10:51:41 -0700

    search input wip

--- components/Header.js
+++ components/Header.js
@@ -1,33 +1,50 @@
-import React from "react";
 import Image from "next/image";
+
 import instagram from "../assets/instagram.png";
 import iglogo from "../assets/iglogo.png";
+
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/solid";
 
 const Header = () => {
   return (
     <div>
-      <div className="flex justify-between max-w-6xl"></div>
-      {/* Left */}
-      <div className="relative hidden lg:inline-grid h-24 w-24">
-        <Image
-          src={instagram}
-          alt="instagram-logo"
-          layout="fill"
-          objectFit="contain"
-        />
-      </div>
+      <div className="flex justify-between max-w-6xl">
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+          <Image
+            src={instagram}
+            alt="instagram-logo-word"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
 
-      <div className="relative w-12 h-12 lg:hidden flex-shrink-0">
-        <Image
-          src={iglogo}
-          alt="instagram-logo"
-          ayout="fill"
-          objectFit="contain"
-        />
+        <div className="relative w-12 h-12 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src={iglogo}
+            alt="instagram-logo"
+            ayout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* Middle - search input field */}
+        <div>
+          <div>
+            <SearchIcon className="h-5 w-5 text-gray-500" />
+          </div>
+          <input type="text" placeholder="Search" />
+        </div>
+
+        {/* Right */}
       </div>
-      {/* Middle */}
-    
-      {/* Right */}
     </div>
   );
 };
