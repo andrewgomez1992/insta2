4c81d8c43ee19f27b1250f850f5f6037832b2d21
Andrew Gomez <drewgomez209@gmail.com>
2022-09-20 11:00:18 -0700

    added tailwindcss/forms to override input styles

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
     <div>
       <div className="flex justify-between max-w-6xl">
         {/* Left */}
-        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image
             src={instagram}
             alt="instagram-logo-word"
@@ -26,7 +26,7 @@ const Header = () => {
           />
         </div>
 
-        <div className="relative w-12 h-12 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-12 lg:hidden flex-shrink-0 cursor-pointer">
           <Image
             src={iglogo}
             alt="instagram-logo"
@@ -36,11 +36,15 @@ const Header = () => {
         </div>
 
         {/* Middle - search input field */}
-        <div>
-          <div>
+        <div className="relative mt-1 p-3 rounded-md">
+          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
             <SearchIcon className="h-5 w-5 text-gray-500" />
           </div>
-          <input type="text" placeholder="Search" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-100 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black"
+          />
         </div>
 
         {/* Right */}
