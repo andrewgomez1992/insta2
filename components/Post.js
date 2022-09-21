b6b1f5969c3897552fa3803c3e24135f45d910cc
Andrew Gomez <drewgomez209@gmail.com>
2022-09-21 16:19:50 -0700

    icons for post imported

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+
+import { HeartIcon as HeartIconFilled } from
+"@heroicons/react/solid"
 
 const Post = ({ id, username, userImg, img, caption }) => {
   return (
