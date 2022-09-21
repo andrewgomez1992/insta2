8f120073d8120b33bf56fb4fb4e6b258013f906f
Andrew Gomez <drewgomez209@gmail.com>
2022-09-21 16:16:54 -0700

    post layout done, props brought in to post

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 const Post = ({ id, username, userImg, img, caption }) => {
   return (
-    <div>Hello post</div>
+    <div>
+      <h1>Post</h1>
+
+      {/* Header */}
+      <div>
+        <img src="" alt="" />
+        <p>{username}</p>
+      </div>
+      {/* img */}
+
+      {/* buttons */}
+
+      {/* caption */}
+
+      {/* comments */}
+
+      {/* input box */}
+
+
+    </div>
   )
 }
 
