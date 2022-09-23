74d62a62116e6c864fe34347aa136e4106d77767
Andrew Gomez <drewgomez209@gmail.com>
2022-09-23 13:40:50 -0700

    firebase and nextauth installed

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7SP-Qxo2Ebuz1Uo64RUn5AcfDz4QYESg",
+  authDomain: "insta2-c1677.firebaseapp.com",
+  projectId: "insta2-c1677",
+  storageBucket: "insta2-c1677.appspot.com",
+  messagingSenderId: "1052158365082",
+  appId: "1:1052158365082:web:addf177b6982a7ee0bbe23"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
