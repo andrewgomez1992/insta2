0466d99e2924130d254cff1dfda61a4627f23f13
Andrew Gomez <drewgomez209@gmail.com>
2022-09-23 23:39:10 -0700

    posts wip

--- components/Posts.js
+++ components/Posts.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import drewselfie from "../assets/drewselfie.png";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 const posts = [
   {
@@ -27,6 +29,18 @@ const posts = [
 ];
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
+      setPosts(snapshot.docs);
+    })
+
+    return () => {
+      unsubscribe();
+    }
+  }, [])
+
   return (
     <div>
       {posts.map((post) => (
