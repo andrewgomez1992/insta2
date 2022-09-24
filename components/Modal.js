40b3adaf7e15275d6c01da685d22fc010cf8a5da
Andrew Gomez <drewgomez209@gmail.com>
2022-09-23 22:11:55 -0700

    upload image connected for modal

--- components/Modal.js
+++ components/Modal.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { Fragment, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
@@ -10,7 +11,14 @@ const Modal = () => {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
 
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
   };
 
   return (
@@ -61,16 +69,26 @@ const Modal = () => {
             overflow-hidden shadow-xl transform-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
             >
               <div>
-                <div
-                  onClick={() => filePickerRef.current.click()}
-                  className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100
-                cursor-pointer"
-                >
-                  <CameraIcon
-                    className="h-6 w-6 text-red-600"
-                    aria-hidden="true"
+                {selectedFile ? (
+                  <img
+                    src={selectedFile}
+                    className='w-full object-contain cursor-pointer'
+                    onClick={() => setSelectedFile(null)}
+                    alt=""
                   />
-                </div>
+                ) : (
+                  <div
+                    onClick={() => filePickerRef.current.click()}
+                    className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100
+                  cursor-pointer"
+                  >
+                    <CameraIcon
+                      className="h-6 w-6 text-red-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                )}
+
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title
