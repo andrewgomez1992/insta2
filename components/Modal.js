8069851fabbfd10dce5b74c70087b32aafd54c64
Andrew Gomez <drewgomez209@gmail.com>
2022-09-23 22:06:42 -0700

    filePickerRef connected to camera icon div

--- components/Modal.js
+++ components/Modal.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
 import { Dialog, Transition } from "@headlessui/react";
@@ -6,6 +6,12 @@ import { CameraIcon } from "@heroicons/react/outline";
 
 const Modal = () => {
   const [open, setOpen] = useRecoilState(modalState);
+  const filePickerRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const addImageToPost = (e) => {
+
+  };
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -56,14 +62,14 @@ const Modal = () => {
             >
               <div>
                 <div
-                  // onClick={() => filePickerRef.current.click()}
+                  onClick={() => filePickerRef.current.click()}
                   className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100
                 cursor-pointer"
                 >
                   <CameraIcon
-                   className="h-6 w-6 text-red-600" 
-                   aria-hidden='true'
-                   />
+                    className="h-6 w-6 text-red-600"
+                    aria-hidden="true"
+                  />
                 </div>
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
@@ -76,9 +82,9 @@ const Modal = () => {
                     <div>
                       <input
                         type="file"
-                        // ref={filePickerRef}
+                        ref={filePickerRef}
                         hidden
-                        // onChange={addImageToPost}
+                        onChange={addImageToPost}
                       />
                     </div>
 
