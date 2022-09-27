3927581a5ea01c467b99a816d368c18d2f405cd9
Andrew Gomez <drewgomez209@gmail.com>
2022-09-27 15:55:58 -0700

    updated secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,7 +11,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  secret: process.env.SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   pages: {
     signIn: "/auth/signin",
   },
